12675bce3639e8fddf4dab7d345641e539263b5c
Viktor Turskyi <viktor@webbylab.com>
2012-08-05 13:15:10 +0300

    0.002 reduce perl version dependency to 0.08

--- lib/DLM/Client.pm
+++ lib/DLM/Client.pm
@@ -64,7 +64,7 @@ our %EXPORT_TAGS = ( 'all' => [ qw(
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
